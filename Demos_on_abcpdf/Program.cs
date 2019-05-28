a3446e4ceb0190c8f88931901a1d28ce401ed0f6
Nuthan Murarysetty <inbox.nuthan@gmail.com>
2019-05-29 03:42:35 +0530

    Code to generate Single Page and Multiple Pages of single Web URL inside PDF.

--- Demos_on_abcpdf/Program.cs
+++ Demos_on_abcpdf/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using WebSupergoo.ABCpdf11;
 
 namespace Demos_on_abcpdf
@@ -7,10 +8,33 @@ namespace Demos_on_abcpdf
         static void Main(string[] args)
         {
             string outputFolder = @"D:\Projects\AbcPDF_Demos\abcpdf-demos\Demos_on_abcpdf\Output\";
-            // Create a basic PDF
-            Doc mainDoc = new Doc(); // This Doc class is coming from WebSupergoo.ABCpdf11 namespace.
-            mainDoc.AddText("Hello!!! My First PDF Demo using abcpdf library");
-            mainDoc.Save(outputFolder + "PDFWithSimpleText.pdf");
+            Doc doc = new Doc(); // This Doc class is coming from WebSupergoo.ABCpdf11 namespace.
+
+            #region Demo 1 Starts
+            //// Create a basic PDF With text
+            //doc.AddText("Hello!!! My First PDF Demo using abcpdf library");
+            //doc.Save(outputFolder + "PDFWithSimpleText.pdf");
+            #endregion Demo 1 Ends
+
+            #region Demo 2 Starts - Single PDF
+            //doc.AddImageUrl("https://techinuthan.blogspot.com/"); // Converts web page to Image in PDF
+            //doc.Save(outputFolder + "ConvertWebPageToPDF.pdf");
+            #endregion Demo 2 Ends - Single PDF
+
+            #region Demo 3 Starts -  Multiple Pages inside single PDF
+            int theID = doc.AddImageUrl("https://techinuthan.blogspot.com/"); // Converts web page to multiple Images in PDF
+            while(true)
+            {
+                if (!doc.Chainable(theID))
+                    break;
+                doc.Page = doc.AddPage();
+                doc.AddImageToChain(theID);
+            }
+            doc.Save(outputFolder + "ConvertWebPageToMultiplePages.pdf");
+            #endregion Demo 3 Ends - Multiple Pages inside single PDF
+
+            Console.WriteLine("PDF Generated Successfully");
+            Console.ReadLine();
         }
     }
 }
